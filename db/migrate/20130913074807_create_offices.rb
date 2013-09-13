f8acd402250dd633a2aec5975edae63113ed430a
poojadd <pooja.dudhatra@gmail.com>
2013-09-14 00:36:32 +0530

    db/

---
+++ db/migrate/20130913074807_create_offices.rb
@@ -0,0 +1,11 @@
+class CreateOffices < ActiveRecord::Migration
+  def change
+    create_table :offices do |t|
+      t.string :name
+      t.float :latitude
+      t.float :longitude
+
+      t.timestamps
+    end
+  end
+end
