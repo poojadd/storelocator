4a712b263fcca3fb106596286ab0102479cf75dd
poojadd <pooja.dudhatra@gmail.com>
2013-09-14 00:31:35 +0530

    conig/

---
+++ config/initializers/secret_token.rb
@@ -0,0 +1,7 @@
+# Be sure to restart your server when you modify this file.
+
+# Your secret key for verifying the integrity of signed cookies.
+# If you change this key, all old signed cookies will become invalid!
+# Make sure the secret is at least 30 characters and all random,
+# no regular words or you'll be exposed to dictionary attacks.
+Storelocator::Application.config.secret_token = '33ed8d827906564d0d2a268add15b31643c330662461f4fe9d918dceaa7738ffcb2e80ac047ed235fe1c39d3e6f59a1922c028836232c995170aa6692c9048e4'
---
+++ config/initializers/session_store.rb
@@ -0,0 +1,8 @@
+# Be sure to restart your server when you modify this file.
+
+Storelocator::Application.config.session_store :cookie_store, key: '_storelocator_session'
+
+# Use the database for sessions instead of the cookie-based default,
+# which shouldn't be used to store highly confidential information
+# (create the session table with "rails generate session_migration")
+# Storelocator::Application.config.session_store :active_record_store
